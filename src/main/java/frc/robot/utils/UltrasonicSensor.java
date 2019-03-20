22d75b0b4d24e2e983225d79f237db43743fed11
Garrett Burroughs <garrettburroughs02@gmail.com>
2019-03-20 11:35:55 -0400

    Started implementation of ultrasonic sensor

--- src/main/java/frc/robot/utils/UltrasonicSensor.java
+++ src/main/java/frc/robot/utils/UltrasonicSensor.java
@@ -2,18 +2,27 @@ package frc.robot.utils;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.SampleRobot;
 import org.montclairrobotics.sprocket.utils.Input;
 
 public class UltrasonicSensor implements Input<Double> {
-    AnalogInput analogInput;
-    
-    public UltrasonicSensor(int channel){
-        analogInput = new AnalogInput(channel);
-    }
+    private AnalogInput sensor;
+    private final int channel;
 
+    public UltrasonicSensor(int channel){
+        this.channel = channel;
+    }
+    public void analogValues(){
+        int raw = sensor.getValue();
+        double volts = sensor.getVoltage();
+    }
+    public void analogInit(){
+        sensor = new AnalogInput(channel);
+
+    }
     @Override
     public Double get() {
-        return analogInput.getVoltage() * 5;
+        double range = sensor.getRangeInches(); //  fix this
+        return range;
     }
-
 }
